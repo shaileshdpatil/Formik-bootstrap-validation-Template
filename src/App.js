36a132d9494e7c92efbf4169c7f90d50511a54d4
Shailesh PatiL <69628770+shaileshdpatil@users.noreply.github.com>
2023-03-16 22:43:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import Login from "./components/Login";
 import AuthProvider from "./components/context/index";
-import i18next from "i18next";
-import { initReactI18next } from "react-i18next";
-import HttpApi from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
+// import i18next from "i18next";
+// import { initReactI18next } from "react-i18next";
+// import HttpApi from "i18next-http-backend";
+// import LanguageDetector from "i18next-browser-languagedetector";
 
-i18next
-  .use(HttpApi)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    supportedLngs: ["en", "ar", "hi", "gr", "fr"],
-    fallbackLng: "en",
-    debug: false,
-    detection: {
-      order: ["path", "cookie", "htmlTag"],
-      caches: ["cookie"],
-    },
-    backend: {
-      loadPath: "/language/{{lng}}.json",
-    },
-});
+// i18next
+//   .use(HttpApi)
+//   .use(LanguageDetector)
+//   .use(initReactI18next)
+//   .init({
+//     supportedLngs: ["en", "ar", "hi", "gr", "fr"],
+//     fallbackLng: "en",
+//     debug: false,
+//     detection: {
+//       order: ["path", "cookie", "htmlTag"],
+//       caches: ["cookie"],
+//     },
+//     backend: {
+//       loadPath: "/language/{{lng}}.json",
+//     },
+// });
+
+// i18next.changeLanguage(option.value);
 
 function App() {
     return (
